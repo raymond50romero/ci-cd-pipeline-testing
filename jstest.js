063eb6ed7b95c6fc4294b04400c530b3e5587ff8
raymond50romero <raymond50romero@users.noreply.github.com>
2022-11-12 06:21:19 +0000

    Prettified Code!

--- jstest.js
+++ jstest.js
@@ -1,12 +1,12 @@
 /**
-   * @swagger
-   * /:
-   *   get:
-   *     description: Returns all users.
-   *     responses:
-   *       200:
-   *         description: All users were returned.
-   */
-function msg(){  
- alert("Hello Javatpoint");  
-}  
+ * @swagger
+ * /:
+ *   get:
+ *     description: Returns all users.
+ *     responses:
+ *       200:
+ *         description: All users were returned.
+ */
+function msg() {
+  alert("Hello Javatpoint");
+}
