019901e8c6eae3c75d243db37e09c9bd8126cf9a
Atomgroup530 <Atomgroup530@users.noreply.github.com>
2022-11-16 04:53:35 +0000

    Prettified Code!

--- testOurCode.js
+++ testOurCode.js
@@ -1,29 +1,24 @@
 //choresListAPI.js
 
 /**
- * Reads 'ChoresListData' data from local storage 
+ * Reads 'ChoresListData' data from local storage
  * and returns a JSON with all the chores information
  * found. If nothing in local storage returns JSON with
  * initialized fields
  * @returns {Object} A JSON object of ChoresListData
  */
-export function readChores(){
-    if(localStorage.getItem("ChoresListData")  === null){
-        let chores = {
-            "chores" : [
-
-            ],
-            "archived" : [
-
-            ],
-            "openChoresCount" : 0,
-            "closedChoresCount" : 0,
-            "choresCountId" : 1
-        }
-        localStorage.setItem("ChoresListData", JSON.stringify(chores));
-        return JSON.parse(localStorage.getItem("ChoresListData"));
-    }
-    else{
-        return JSON.parse(localStorage.getItem("ChoresListData"));
-    }
+export function readChores() {
+  if (localStorage.getItem("ChoresListData") === null) {
+    let chores = {
+      chores: [],
+      archived: [],
+      openChoresCount: 0,
+      closedChoresCount: 0,
+      choresCountId: 1,
+    };
+    localStorage.setItem("ChoresListData", JSON.stringify(chores));
+    return JSON.parse(localStorage.getItem("ChoresListData"));
+  } else {
+    return JSON.parse(localStorage.getItem("ChoresListData"));
+  }
 }
